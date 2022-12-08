342ea0a4f3e34a579908fd25232daea8c21e8f89
Clement Jean <clement.jean@epitech.eu>
2022-12-08 12:21:23 +0800

    function to get current git branch

---
+++ .emacs.d/lisp/git.el
@@ -0,0 +1,8 @@
+(defun git-branch-name ()
+  "Get current git branch name"
+  (let ((args '("symbolic-ref" "HEAD" "--short")))
+    (with-temp-buffer
+      (apply #'process-file "git" nil (list t nil) nil args)
+      (unless (bobp)
+        (goto-char (point-min))
+        (buffer-substring-no-properties (point) (line-end-position))))))
